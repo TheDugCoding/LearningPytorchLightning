c815757fd2334599f4acc17f563b562e88960d8f
Dug <lucadughera@gmail.com>
2025-08-04 12:23:53 +0200

    mid-day break, added Numpy theory until ufunc and exercises

---
+++ src/NumPy/exercises.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+#create a simple array with 3 float numbers
+simple_array = np.array([10, 32, 12.56], dtype=np.float32)
+print(simple_array)
+
+#print an array that goes from 10 to 100 with step 10
+simple_array_range = np.array(range(10, 101, 10))
+print(simple_array_range)
+simple_array_arange = np.arange(31)
+print('arange\n', simple_array_range)
+
+#try using a list comprehension for an array from 10 to 30, step 5
+simple_array_list_comprehension = np.array([x for x in range(10, 31, 5)])
+print(simple_array_list_comprehension)
+
+#create an array of zeroes one, two and three dimensions
+array_zeroes_one_dimension = np.zeros(3)
+print(array_zeroes_one_dimension)
+array_zeroes_two_dimensions = np.zeros([4,4])
+print(array_zeroes_two_dimensions)
+# you can use tuples or lists for the size it doesn't matter
+array_zeroes_three_dimensions = np.zeros((5,5,5),dtype=int)
+print(array_zeroes_three_dimensions)
+
+#create an array of ones
+array_ones_simple = np.ones((6,6), dtype=int)
+print(array_ones_simple)
+
+#create an array of numbers equally separated
+array_numbers_equally_separated = np.linspace(0, 10, 20)
+print(array_numbers_equally_separated)
+
+#for the last array find dimension, size and total number of elements
+print(array_numbers_equally_separated.ndim)
+print(array_numbers_equally_separated.shape)
+print(array_numbers_equally_separated.size)
+
+#selecting and slicing
+
+array_two_dimensions = np.array([range(x, x+11) for x in [10, 20, 30]])
+print(array_two_dimensions)
+#selecting a single element
+print('single element\n', array_two_dimensions[0,0])
+#slicing, select first and third row
+print('slicing, select first and third row\n', array_two_dimensions[[0,2]])
+#select from 5Th column from 2nd row
+print('slicing from 2nd row from 5th column\n', array_two_dimensions[1:, 5:])
+#all rows except column 3
+print('all rows except column 3\n', array_two_dimensions[:, [i for i in range(array_two_dimensions.shape[1]) if i != 2]])
---
+++ src/NumPy/numpy_how_to.py
@@ -0,0 +1,117 @@
+import numpy as np
+
+#the problem with python is that lists are very flexible but inefficient, each element in a list is an
+# object, so a list is a list of references to different objects, these objects may also
+# be of different types(dynamic typing, in python we don't need to specify the type of a variable).
+# Moreover, these objects are not in contiguous blocks.
+#numpy offers a solution for this by simplifying how values are stored, an array contains only entries of the same
+#type, so numpy is more efficient because it doesn't need to check for type compatibility
+# and provides more functionalities compared to python, but it loses flexibility
+
+
+#compared to python arrays numpy give the user access to more advanced operations
+int_array = np.array([1, 4, 2, 5, 3])
+print(int_array)
+
+#we can also decide the type of the array
+float_array = np.array([1, 2, 3, 4], dtype='float32')
+print(float_array)
+
+#also they can be multidimensional
+print([range(i, i + 3) for i in [2, 4, 6]])
+matrix_int = np.array([range(i, i + 3) for i in [2, 4, 6]])
+print(matrix_int)
+
+# Create a length-10 integer array filled with zeros
+print(np.zeros(10, dtype=int))
+
+# Create a 3x5 floating-point array filled with 1s
+print(np.ones((3, 5), dtype=float))
+
+# Create a 3x5 array filled with 3.14
+print(np.full((3, 5), 3.14))
+
+# Create an array filled with a linear sequence
+# Starting at 0, ending at 20, stepping by 2
+# (this is similar to the built-in range() function)
+print(np.arange(0, 20, 2))
+
+# Create an array of five values evenly spaced between 0 and 1
+print(np.linspace(0, 1, 5))
+
+# Create a 3x3 array of uniformly distributed
+# random values between 0 and 1
+print(np.random.random((3, 3)))
+
+#if we use random we can set a seed for reproducibility
+np.random.seed(0)
+
+#to check an array size we can use these three attributes
+# .ndim for the number of dimensions
+# .shape for the size of each dimension
+# .size for the total size of the array (total number of entries)
+
+#accessing elements is similar to lists
+array_accessing = np.ones((10,10))
+print(array_accessing[5,5])
+
+#in a multidimensional array you can use slicing like this x[start:stop:step]
+#there is also x[5::7] start from 5 but ignore 7
+
+#VERY IMPORTANT, if we put a subarray in a variable and then we modify the subarray the changes will also
+#apply to the original array, use .copy() to change elements without affecting the original array
+
+#reshaping, means change the structure of an array, the size of the initial array must be the same of the final array
+grid = np.arange(1, 10).reshape((3, 3))
+print(grid)
+
+#concatenate np.concatenate, np.vstack, and np.hstack
+x = np.array([1, 2, 3])
+y = np.array([3, 2, 1])
+print(x, y)
+print('concatenate', np.concatenate([x, y]))
+
+grid = np.array([[1, 2, 3],
+                [4, 5, 6]])
+
+# concatenate along the first axis
+print('concatenate along the first axis', np.concatenate([grid, grid]))
+# concatenate along the second axis
+print('concatenate along the second axis', np.concatenate([grid, grid], axis = 1))
+
+#but for multidimensional stacking is better to use np.vstack (vertical stack) and np.hstack (horizontal stack)
+
+#splitting is implemented by the functions np.split, np.hsplit, and np.vsplit
+
+
+'''---Ufunc, ---'''
+
+#python is slow with repeated operations, because it needs to check the type of the elements every time because it
+#assigns the correct function for the type
+# numpy solves this problem with vectorized operations, it skips the type checking because a numpy array
+# contains only one type of elements. Vectorized operations are implemented through ufuncs
+# depending on the size of the input we have ufuncs(one input) or binary ufuncs(two inputs)
+# ufuncs use the same operator as in python
+
+x = np.arange(4)
+print("x =", x)
+print("x + 5 =", x + 5)
+print("x - 5 =", x - 5)
+print("x * 2 =", x * 2)
+print("x / 2 =", x / 2)
+print("x // 2 =", x // 2) # floor division
+
+'''
+operators and np variation are roughly the same
++ np.add Addition (e.g., 1 + 1 = 2)
+- np.subtract Subtraction (e.g., 3 - 2 = 1)
+- np.negative Unary negation (e.g., -2)
+* np.multiply Multiplication (e.g., 2 * 3 = 6)
+/ np.divide Division (e.g., 3 / 2 = 1.5)
+// np.floor_divide Floor division (e.g., 3 // 2 = 1)
+** np.power Exponentiation (e.g., 2 ** 3 = 8)
+% np.mod Modulus/remainder (e.g., 9 % 4 = 1)
+np.log(x)
+np.exp(x)
+np.power(3, x)
+'''
